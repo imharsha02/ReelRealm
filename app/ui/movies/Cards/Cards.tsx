ceb99663d454d2751233caa87f0bdbda748acb2c
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-12-28 15:27:14 +0530

    Made individual movie page, the loading state of movie and list of movies responsive

--- app/ui/movies/Cards/Cards.tsx
+++ app/ui/movies/Cards/Cards.tsx
@@ -10,11 +10,11 @@ export default async function Card({
   Image_source: string;
 }) {
   return (
-    <div className="p-5 space-y-2 bg-slate-100 lg:w-96 rounded-md">
+    <div className="p-5 space-y-2 bg-slate-100 w-80 lg:w-96 rounded-md">
       <div className="flex items-center justify-center">
         <Image src={Image_source} alt="" height={200} width={200} />
       </div>
-      <div className="">
+      <div>
         <h3 className={`flex flex-wrap justify-center text-center ${kanit.className}`}>{name}</h3>
       </div>
     </div>
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -3,20 +3,20 @@ const shimmer =
 
 export function MovieSkeleton() {
   return (
-    <div className={`${shimmer} my-5 mx-auto w-max space-y-3`}>
-      <div className="rounded-lg p-3 bg-slate-100 border-none h-[600px] w-[600px]"></div>
+    <div className={`${shimmer} my-5 mx-auto md:w-max space-y-3`}>
+      <div className="rounded-lg p-3 bg-slate-200 border-none h-[600px] w-[600px]"></div>
       <div className="w-max mx-auto">
         <div className="flex gap-1 items-center mb-1">
-          <div className="h-5 w-20  bg-slate-100"></div>
-          <div className="h-4 w-20  bg-slate-100"></div>
+          <div className="h-5 w-20  bg-slate-200"></div>
+          <div className="h-4 w-20  bg-slate-200"></div>
         </div>
         <div className="flex gap-1 items-center mb-1">
-          <div className="h-5 w-20  bg-slate-100"></div>
-          <div className="h-4 w-20  bg-slate-100"></div>
+          <div className="h-5 w-20  bg-slate-200"></div>
+          <div className="h-4 w-20  bg-slate-200"></div>
         </div>
         <div className="flex gap-1 items-center mb-1">
-          <div className="h-5 w-20  bg-slate-100"></div>
-          <div className="h-4 w-20  bg-slate-100"></div>
+          <div className="h-5 w-20  bg-slate-200"></div>
+          <div className="h-4 w-20  bg-slate-200"></div>
         </div>
       </div>
       <div className="mx-auto flex justify-between mt-3">
