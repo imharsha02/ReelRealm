388913b2bb32e7471c236192261c1a49042cef3b
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-12-30 13:47:14 +0530

    Added animation to skeleton while loading of individual movie

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,27 +1,24 @@
-const shimmer =
-  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
-
 export function MovieSkeleton() {
   return (
-    <div className={`${shimmer} my-5 mx-auto md:w-max space-y-3`}>
-      <div className="rounded-lg p-3 bg-slate-200 border-none h-[600px] w-[600px]"></div>
+    <div className={`my-5 mx-auto md:w-max space-y-3`}>
+      <div className="shimmer rounded-lg p-3 bg-slate-200 border-none h-[600px] w-[600px]"></div>
       <div className="w-max mx-auto">
         <div className="flex gap-1 items-center mb-1">
-          <div className="h-5 w-20  bg-slate-200"></div>
-          <div className="h-4 w-20  bg-slate-200"></div>
+          <div className="h-5 w-20  bg-slate-200 shimmer"></div>
+          <div className="h-4 w-20  bg-slate-200 shimmer"></div>
         </div>
         <div className="flex gap-1 items-center mb-1">
-          <div className="h-5 w-20  bg-slate-200"></div>
-          <div className="h-4 w-20  bg-slate-200"></div>
+          <div className="h-5 w-20  bg-slate-200 shimmer"></div>
+          <div className="h-4 w-20  bg-slate-200 shimmer"></div>
         </div>
         <div className="flex gap-1 items-center mb-1">
-          <div className="h-5 w-20  bg-slate-200"></div>
-          <div className="h-4 w-20  bg-slate-200"></div>
+          <div className="h-5 w-20  bg-slate-200 shimmer"></div>
+          <div className="h-4 w-20  bg-slate-200 shimmer"></div>
         </div>
       </div>
       <div className="mx-auto flex justify-between mt-3">
-        <div className="bg-slate-100 rounded-lg px-5 py-3 w-20 h-4"></div>
-        <div className="bg-slate-100 rounded-lg px-5 py-3 w-20 h-4"></div>
+        <div className="bg-slate-100 rounded-lg px-5 py-3 w-20 h-4 shimmer"></div>
+        <div className="bg-slate-100 rounded-lg px-5 py-3 w-20 h-4 shimmer"></div>
       </div>
     </div>
   );
